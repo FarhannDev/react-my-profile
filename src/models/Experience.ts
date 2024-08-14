01c4b7f0fc672d7b83a3f2ffe4393c6f221d4a00
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2024-08-14 09:33:05 +0700

    Modified

--- src/models/Experience.ts
+++ src/models/Experience.ts
@@ -9,19 +9,43 @@ export interface Experience {
 
 const experience: Experience[] = [
   {
+    company: 'PT.Winnicode Garuda Indonesia',
+    job: 'Web Developer Laravel',
+    location: 'Bandung, Jawa Barat, Indonesia',
+    startDate: 'Aug 2024',
+    endDate: 'Sekarang',
+    description: ``,
+  },
+  {
     company: 'PT Citi Asia Internasional',
     job: 'Full Stack Developer',
-    location: 'JAKARTA,INDONESIA',
+    location: 'Jakarta Selatan, Jakarta Raya, Indonesia',
     startDate: 'Aug 2023',
-    endDate: 'DEC 2023',
+    endDate: 'Des 2023',
     description: ``,
   },
   {
     company: 'PT SOLUSI TIGA SELARAS',
     job: 'Full Stack Laravel Developer',
-    location: 'JAKARTA,INDONESIA',
+    location: 'Jakarta Selatan, Jakarta Raya, Indonesia',
     startDate: 'Aug 2022',
-    endDate: 'DEC 2022',
+    endDate: 'Des 2022',
+    description: ``,
+  },
+  {
+    company: 'Universitas Bina Sarana Informatika',
+    job: 'Volunteer Workshop DevOps Menggunakan Kubernetes (K8s)',
+    location: '',
+    startDate: 'Jun 2022',
+    endDate: 'Jun 2022',
+    description: ``,
+  },
+  {
+    company: 'PT Bipie',
+    job: 'Digital Marketing Intern',
+    location: 'Tambun Selatan, Jawa Barat, Indonesia',
+    startDate: 'Sep 2018',
+    endDate: 'Mar 2019',
     description: ``,
   },
 ];
