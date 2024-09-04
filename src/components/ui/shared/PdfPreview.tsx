2c455c14bf5cbeec3230024091666ee68176d63b
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2024-09-04 07:30:47 +0700

    Modified pdf preview

--- src/components/ui/shared/PdfPreview.tsx
+++ src/components/ui/shared/PdfPreview.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import {
+  DefaultLayoutPlugin,
+  defaultLayoutPlugin,
+} from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 type IProps = { fileUrl: string | undefined };
 
 const PdfPreview: React.FC<IProps> = ({ fileUrl }) => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const defaultLayoutPluginInstance: DefaultLayoutPlugin =
+    defaultLayoutPlugin();
+  const workerUrl: string =
+    'https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js';
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      <Worker
-        workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
-      >
+      <Worker workerUrl={workerUrl}>
         {fileUrl && (
-          <Viewer fileUrl={fileUrl} plugins={[defaultLayoutPluginInstance]} />
+          <Viewer
+            fileUrl={fileUrl}
+            plugins={[defaultLayoutPluginInstance]}
+            enableSmoothScroll
+            defaultScale={1.0}
+            theme={'dark'}
+          />
         )}
       </Worker>
     </div>
